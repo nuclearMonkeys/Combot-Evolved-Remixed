36fcc0903c5e683372db7fc236097cc3b2e4205b
nuclearMonkeys <lyndond@uci.edu>
2019-11-04 11:48:57 -0800

    Added Blocks and Trail Renderer

--- Combat-Evolved/Assets/_Scripts/PlayerController.cs
+++ Combat-Evolved/Assets/_Scripts/PlayerController.cs
@@ -5,28 +5,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Tank Variables")]
+    [Header("Tank GameObjects")]
     public PlayerInput playerInput;
-    [SerializeField] private float m_movementSpeed = 5.0f;
+
     [SerializeField] private GameObject body;
     [SerializeField] private GameObject head;
     [SerializeField] private GameObject firePoint;
     [SerializeField] private DefaultBullet bulletPrefab;
-    [SerializeField] private float deadzone = 0.01f;
+
+    [Header("Player Variables")]
+    [SerializeField] private float m_movementSpeed = 5.0f;
     [SerializeField] private Vector2 direction;
 
     [Header("Dash Variables")]
     [SerializeField] private float dashSpeed;
     [SerializeField] private float startDashTime;
-    public bool isDashing = false;
-    public float dashTime;
     
-
+    // private variables. no touchy touchy.
+    private bool isDashing = false;
+    private float dashTime;
+    
     public int maxCooldown = 60;
-    public int cooldown = 0;
+    private int cooldown = 0;
 
-    public Rigidbody2D m_rigidbody;
+    private Rigidbody2D m_rigidbody;
     private Vector2 rotation;
+    private TrailRenderer trailRenderer;
+
+    private LineRenderer line;
 
     void Awake()
     {
@@ -36,15 +42,21 @@ public class PlayerController : MonoBehaviour
     void Start() 
     {
         // Ugly ass code
-        body =        GetComponentsInChildren<Transform>()[1].gameObject;
-        head =        GetComponentsInChildren<Transform>()[2].gameObject;
-        firePoint =   GetComponentsInChildren<Transform>()[4].gameObject;
-        m_rigidbody = GetComponent<Rigidbody2D>();
+        body =        this.GetComponentsInChildren<Transform>()[1].gameObject;
+        head =        this.GetComponentsInChildren<Transform>()[2].gameObject;
+        firePoint =   this.GetComponentsInChildren<Transform>()[4].gameObject;
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        trailRenderer = this.GetComponent<TrailRenderer>();
+        line = this.GetComponent<LineRenderer>();
+        // dottedLine = new DottedLine();
+        // trailRenderer.enabled = !trailRenderer.enabled;
         dashTime = startDashTime;
     }
 
     void Update() 
     {
+        DrawLine();
+
         if(cooldown > 0)
             cooldown--;
 
@@ -66,33 +78,88 @@ public class PlayerController : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context) 
     {
-        StartCoroutine(dashEnumerator(context));
+        StartCoroutine(boostEnumerator(context));
     }
 
     public void Rotate(InputAction.CallbackContext context) 
     {
+        // When you release stick on resting place
+        Vector2 previousRotation = rotation;
         rotation = context.ReadValue<Vector2>();
 
-        float h = rotation.x;
-        float v = rotation.y;
+        float h = 0.0f;
+        float v = 0.0f;
 
+        //print(rotation);
+
+        if(rotation != Vector2.zero && withinThreshold(rotation)) {
+            h = rotation.x;
+            v = rotation.y;
+        } else {
+            h = previousRotation.x;
+            v = previousRotation.y;
+        }
+
+        
         float aim_angle = Mathf.Atan2(v, h) * Mathf.Rad2Deg;
         head.transform.rotation = Quaternion.AngleAxis(aim_angle, Vector3.forward);
     }
 
     public void Dash(InputAction.CallbackContext context) 
     {
-        if(dashTime <= 0)
+        if(dashTime <= 0 && direction != Vector2.zero)
             isDashing = true;
+    }
+
+    IEnumerator boostEnumerator(InputAction.CallbackContext context) 
+    {
+    
+        // This allows the player to control the direction of the 'dash'
+        Vector2 previousDirection = direction;
+        direction = context.ReadValue<Vector2>();
+
+        // print(direction);
+
+        float h = 0.0f;
+        float v = 0.0f;
+
+        if(direction != Vector2.zero && withinThreshold(direction)) {
+            h = direction.x * m_movementSpeed;
+            v = direction.y * m_movementSpeed;
+        } else {
+            h = previousDirection.x * m_movementSpeed;
+            v = previousDirection.y * m_movementSpeed;
+        }
+        
+        // direction = previousDirection;
+            
+        float aim_angle = Mathf.Atan2(v, h) * Mathf.Rad2Deg;
+        body.transform.rotation = Quaternion.AngleAxis(aim_angle, Vector3.forward);
+
+        if(dashTime <= 0 && isDashing) {
+            // trailRenderer.enabled = !trailRenderer.enabled;
+            m_rigidbody.velocity = direction * dashSpeed;
+            yield return new WaitForSecondsRealtime(0.5f);
+            dashTime = startDashTime;
+            isDashing = false;
+        }
+        else {
+            m_rigidbody.velocity = direction * m_movementSpeed;
+        }
+            
+        yield return null;
     }
 
     IEnumerator dashEnumerator(InputAction.CallbackContext context) 
     {
+        // This is for a regular dash. Planned for testing.
+        // Not for final use.
         direction = context.ReadValue<Vector2>();
         float h = direction.x * m_movementSpeed;
         float v = direction.y * m_movementSpeed;
 
-        // If you want a regular dash
+        print(direction);
+
         if(dashTime <= 0 && isDashing) {
             m_rigidbody.velocity = direction * dashSpeed;
             dashTime = startDashTime;
@@ -108,22 +175,38 @@ public class PlayerController : MonoBehaviour
             body.transform.rotation = Quaternion.AngleAxis(aim_angle, Vector3.forward);
         }
 
-        // If you want a "curved" dash
-        /*
-            if(dashTime <= 0 && isDashing) {
-                m_rigidbody.velocity = direction * dashSpeed;
-                dashTime = startDashTime;
-                yield return new WaitForSecondsRealtime(0.03f);
-                isDashing = false;
-            }
-            else {
-                m_rigidbody.velocity = direction * m_movementSpeed;
-            }
-
-            float aim_angle = Mathf.Atan2(v, h) * Mathf.Rad2Deg;
-            body.transform.rotation = Quaternion.AngleAxis(aim_angle, Vector3.forward);
-         */
-
         yield return null;
+    }
+
+    // I've spent way too long to make dotted lines
+    // Come back to this later
+    private void DrawLine() 
+    {
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.transform.position, firePoint.transform.right);
+
+        // Debug.DrawLine(firePoint.transform.position, hit.point);
+        // dottedLine.DrawDottedLine(firePoint.transform.position, hit.point);
+        
+        line.SetPosition(0, firePoint.transform.position);
+
+        if(hit)
+            line.SetPosition(1, hit.point);
+        else {
+            line.SetPosition(1, firePoint.transform.position * 2/0.4f);
+        }
+        
+    }
+
+    /*
+     * Determines if the absolute value of x and y coordinates
+     * of the left stick position (direction) is
+     * greater than the value of 
+     * some threshold in the function
+    */
+    private bool withinThreshold(Vector2 vec) 
+    {
+        float threshold = 0.5f;
+        return Mathf.Abs(vec.x) > threshold &&
+             Mathf.Abs(vec.y) > threshold;
     }
 }
---
+++ Combat-Evolved/Assets/_Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public FloatVariable currentHP;
+
+    public bool resetHP;
+    public FloatReference maxHP;
+    public UnityEvent damageEvent;
+    public UnityEvent deathEvent;
+
+    void Start() 
+    {
+        if (resetHP)
+            currentHP.SetValue(maxHP);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        DamageDealer damage = other.gameObject.GetComponent<DamageDealer>();
+        if(damage != null) 
+        {
+            currentHP.ApplyChange(-damage.damageAmount);
+            damageEvent.Invoke();
+        }
+        if(currentHP.Value <= 0.0f)
+        {
+            deathEvent.Invoke();
+        }
+    }
+
+}
