3ccb1100feb382c53041d0477f7a840ae77c8d16
nuclearMonkeys <lyndond@uci.edu>
2020-01-27 11:09:08 -0800

    Added Preliminary Lobby Scene
    Still not complete

---
+++ Combat-Evolved/Assets/_Scripts/ScriptableObjects/CubeRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotate : MonoBehaviour
+{
+    private int cooldown = 25;
+
+    void Update ()
+    {
+        if( cooldown == 0 )
+        {
+            StartCoroutine( RotateAround( Vector3.right, 90.0f, 1.0f) );
+        }
+        cooldown--;
+    }
+ 
+    IEnumerator RotateAround( Vector3 axis, float angle, float duration )
+    {
+        float elapsed = 0.0f;
+        float rotated = 0.0f;
+        while( elapsed < duration ) {
+            float step = angle / duration * Time.deltaTime;
+            transform.RotateAround(transform.position, axis, step );
+            elapsed += Time.deltaTime;
+            rotated += step;
+            yield return null;
+        }
+        transform.RotateAround(transform.position, axis, angle - rotated );
+        cooldown = 120;
+    }
+}
