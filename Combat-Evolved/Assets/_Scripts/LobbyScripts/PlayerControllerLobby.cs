cb93e203ce35c505c7a4ad633ff7aa148ade526b
nuclearMonkeys <lyndond@uci.edu>
2020-01-27 16:58:32 -0800

    Changed WallTile prefab size

---
+++ Combat-Evolved/Assets/_Scripts/LobbyScripts/PlayerControllerLobby.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerLobby : MonoBehaviour
+{
+    public void Activate(InputAction.CallbackContext context) 
+    {
+        print(context);
+        print("ACTIVATE");
+    }
+}
