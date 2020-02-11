4153f1d7b61517c6bda549ed176c3bf95fba37ae
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-02-11 15:38:11 -0800

    fixed layers

--- Combat-Evolved/Assets/_Scripts/ManagerScripts/LayerManager.cs
+++ Combat-Evolved/Assets/_Scripts/ManagerScripts/LayerManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 // Creates constants for layers.
 public class LayerManager : MonoBehaviour
 {
+    public static int DAMAGER = 8;
     public static int TANKBODY = 9;
     public static int TANKWALLCOLLIDER = 10;
     public static int BLOCK = 11;
+    public static int CIRCLE = 12;
     public static int STAGEHAZARD = 13;
 }
