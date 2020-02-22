e3529574e8a9ecca12161c6283300b75bf1504cf
nuclearMonkeys <lyndond@uci.edu>
2020-02-21 17:05:15 -0800

    Teleport bullet passive complete

--- Combat-Evolved/Assets/_Scripts/PlayerScripts/Passives/TeleportPassive.cs
+++ Combat-Evolved/Assets/_Scripts/PlayerScripts/Passives/TeleportPassive.cs
@@ -6,6 +6,7 @@ public class TeleportPassive : PassiveBase
 {
     public GameObject firepoint;
     public PlayerController owner;
+    [SerializeField] private GameObject bullet;
 
     public string soundEffect;
 
@@ -13,17 +14,41 @@ public class TeleportPassive : PassiveBase
     void Start()
     {
         owner = GetComponentInParent<PlayerController>();
+        firepoint = GameObject.Find("Barrel/FirePoint");
     }
 
     public override void ActivatePassive(PlayerController pc) 
     {
-        FireBullet(pc.gameObject.GetComponent<PlayerWeapons>().bulletPrefab);
+        if (!bullet)
+            FireBullet(pc.gameObject.GetComponent<PlayerWeapons>().bulletPrefab);
+        else if (bullet)
+            Teleport();
     }
 
     public void FireBullet(BulletBase bulletPrefab) 
     {
+        // in case there's a pre-existing bullet
+        if (bullet)
+            return;
+        
+        // I'm so sorry
         BulletBase clone = Instantiate(bulletPrefab.gameObject, firepoint.transform.position, transform.rotation).GetComponent<BulletBase>();
+        GameObject barrel = GameObject.Find("Barrel");
+
+        bullet = clone.gameObject;
+
         clone.damage = 0;
         clone.source = owner;
+        clone.SetDirection( (clone.gameObject.transform.position - barrel.transform.position).normalized );
+        cooldown = 0;
+    }
+
+    public void Teleport() 
+    {
+        if (!bullet)
+            return;
+        owner.transform.position = bullet.transform.position;
+        Destroy(bullet.GetComponent<BulletBase>().boxCollider);
+        Destroy(bullet);
     }
 }
