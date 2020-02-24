76b967b6ff7f276c69ca3d59c2d25fa4b0f959ce
Dane Carstens <dcarsten@uci.edu>
2020-02-24 15:34:33 -0800

    made glue hazard

--- Combat-Evolved/Assets/_Scripts/DaneScripts/glueHazard.cs
+++ Combat-Evolved/Assets/_Scripts/DaneScripts/glueHazard.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class glueHazard : MonoBehaviour
 {
+    float speed = -1f;
+
+    public float speedReducer = 0.33f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,20 @@ public class glueHazard : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (speed == -1)
+        {
+            speed = collision.GetComponentInParent<PlayerController>().GetMovementSpeed();
+        }
+        
+        collision.GetComponentInParent<PlayerController>().SetMovementSpeed(speed * speedReducer);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.GetComponentInParent<PlayerController>().SetMovementSpeed(speed);
     }
 }
