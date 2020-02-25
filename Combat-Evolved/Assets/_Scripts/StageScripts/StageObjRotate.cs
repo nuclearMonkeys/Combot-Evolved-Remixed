886571f898d66cc7d3254217cd9bfd0c122ade53
nuclearMonkeys <lyndond@uci.edu>
2020-02-24 16:18:44 -0800

    Push Passive done I think

---
+++ Combat-Evolved/Assets/_Scripts/StageScripts/StageObjRotate.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageObjRotate : MonoBehaviour
+{
+    [Header("Speed Variables")]
+    
+    public float normalSpeed = 50f;
+    public float hitSpeed = 100;
+    public float currentSpeed;
+    public float regenerateInterval = 1f;
+    public float updateSpeedSecs = 5f;
+
+    [Header("Rotate Variables")]
+    public bool constantRotate;
+    public bool x;
+    public bool y;
+    public bool z;
+
+    private bool isHit = false;
+    private float elapsed;
+    Coroutine accelRoutine;
+
+    void Start() 
+    {
+        // currentSpeed = normalSpeed;
+    }
+
+    void Update()
+    {
+        if (!constantRotate && !isHit)
+            return;
+
+        transform.Rotate(
+            x ? currentSpeed * Time.deltaTime : 0,
+            y ? currentSpeed * Time.deltaTime : 0, 
+            z ? currentSpeed * Time.deltaTime : 0 );
+    }
+
+    public void HitObj() 
+    {
+        if (!isHit)
+            Decelerate();
+        else if (isHit) 
+        {
+            elapsed = 0;
+            currentSpeed = hitSpeed;
+        }
+    }
+
+    private void Decelerate() 
+    {
+        isHit = true;
+        accelRoutine = StartCoroutine(DecelerateRoutine());
+    }
+
+    private IEnumerator DecelerateRoutine() 
+    {
+        elapsed = 0;
+        while (elapsed < updateSpeedSecs ||
+            constantRotate ? true : Mathf.Floor(gameObject.transform.eulerAngles.y % 180) > 1) 
+        {
+            elapsed += Time.deltaTime / 2;
+            currentSpeed = Mathf.Lerp(hitSpeed, normalSpeed, elapsed / updateSpeedSecs);
+            yield return new WaitForEndOfFrame();
+        }
+        accelRoutine = null;
+        currentSpeed = normalSpeed;
+        isHit = false;
+        
+    }
+}
