c0caeb6f0e4a15cc692d250bcedb25bcba6b683e
nuclearMonkeys <lyndond@uci.edu>
2020-02-26 15:33:32 -0800

    Added Laser Hazard

---
+++ Combat-Evolved/Assets/_Scripts/StageScripts/LaserHazard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserHazard : HazardDamage
+{
+    public GameObject point1;
+    public GameObject point2;
+    public LineRenderer laser;
+
+    void LateUpdate()
+    {
+        laser.SetPosition(0, point1.transform.position);
+        laser.SetPosition(1, point2.transform.position);
+
+        float distance = (point1.transform.position - point2.transform.position).magnitude;
+        RaycastHit2D playerhit = Physics2D.Raycast(point1.transform.position, -transform.up, distance);
+        print(playerhit.collider);
+        Debug.DrawLine(point1.transform.position, point2.transform.position, Color.blue);
+        
+        PlayerHealth playerHealth = playerhit.collider.GetComponent<PlayerHealth>();
+        if (playerhit && playerHealth) 
+        {
+            playerHealth.TakeDamage(damage * Time.deltaTime);
+            laser.SetPosition(1, playerhit.point);
+        }
+    }
+}
